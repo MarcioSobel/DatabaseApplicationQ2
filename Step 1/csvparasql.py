1e61697bef0d251be0aab03db8e39acbb0c8eba4
carlinaceo28 <carlosalbertoneto28@gmail.com>
2024-05-05 23:37:13 -0300

    .csv p/ .sqp script corrigido

--- Step 1/csvparasql.py
+++ Step 1/csvparasql.py
@@ -10,10 +10,11 @@ moves = pd.read_csv(f"{data_directory}/ability.csv")
 stats = pd.read_csv(f"{data_directory}/stats.csv")
 
 pokemons = pd.merge(pokemons, moves, on='pokedex_nr')
-pokemons = pd.merge(pokemons, stats, on= 'pokedex_nr')
+pokemons_final = pd.merge(pokemons, stats, on= 'pokedex_nr')
+pokemons_final.drop(index=0)
 
 tablename = "tabela_de_pokemons"
-with open(f'{tablename}.sql', 'w', encoding="utf-8") as file:
-        for _, row in pokemons.iterrows():
+with open(f'{tablename}.sql', 'w', encoding='utf-8') as file:
+        for _, row in pokemons_final.iterrows():
             query = f"INSERT INTO {tablename} ({', '.join(pokemons.columns)}) VALUES ({', '.join(row.astype(str).values)});\n"
             file.write(query)
