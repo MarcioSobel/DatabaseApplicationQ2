0dc9f1f64a8f2cb20e4a300756752153425f5f81
marciosobel <marcinhosobel@hotmail.com>
2024-05-13 00:27:42 -0300

    add pokemon_has_types relationship

--- src/mySqlToMongo.py
+++ src/mySqlToMongo.py
@@ -8,6 +8,20 @@ def mySQLtoMongoDB():
     types = get_table("types")
     moves = get_table("moves")
 
+    pokemon_has_types = get_table("pokemon_has_types")
+
+    ######### HANDLE RELATIONSHIPS #########
+    # https://stackoverflow.com/questions/64235312/how-to-implodereverse-of-pandas-explode-based-on-a-column
+    # pokemon_has_types
+    pokemon = pd.merge(pokemon, pokemon_has_types, left_on="pokedex_number", right_on="pokemon_id")
+    temp = pokemon.groupby(["pokedex_number", "pokemon"]).agg({"type_id": list}).reset_index()
+    temp = temp.drop(columns=["pokemon"]).rename(columns={"type_id": "type_ids"})
+    pokemon = pd.merge(pokemon, temp, on="pokedex_number").drop(columns=["pokemon_id", "type_id"]).drop_duplicates(subset=["pokedex_number"])
+
+    ######### RENAME ID COLUMNS TO _ID #########
+    pokemon = pokemon.rename(columns={"pokedex_number": "_id"})
+    types = types.rename(columns={"id": "_id"})
+
     ######### CONVERT FROM DATAFRAME TO DICTS #########
     pokemon = pokemon.to_dict("records")
     types = types.to_dict("records")
