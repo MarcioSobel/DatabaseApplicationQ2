3417d6520f695541e35bf0364e60ec066c923c20
marciosobel <marcinhosobel@hotmail.com>
2024-05-13 00:32:19 -0300

    add pokemon_has_moves relationship

--- src/mySqlToMongo.py
+++ src/mySqlToMongo.py
@@ -9,6 +9,7 @@ def mySQLtoMongoDB():
     moves = get_table("moves")
 
     pokemon_has_types = get_table("pokemon_has_types")
+    pokemon_has_moves = get_table("pokemon_has_moves")
 
     ######### HANDLE RELATIONSHIPS #########
     # https://stackoverflow.com/questions/64235312/how-to-implodereverse-of-pandas-explode-based-on-a-column
@@ -17,14 +18,23 @@ def mySQLtoMongoDB():
     temp = pokemon.groupby(["pokedex_number", "pokemon"]).agg({"type_id": list}).reset_index()
     temp = temp.drop(columns=["pokemon"]).rename(columns={"type_id": "type_ids"})
     pokemon = pd.merge(pokemon, temp, on="pokedex_number").drop(columns=["pokemon_id", "type_id"]).drop_duplicates(subset=["pokedex_number"])
+    
+    # pokemon_has_moves
+    pokemon = pd.merge(pokemon, pokemon_has_moves, left_on="pokedex_number", right_on="pokemon_id")
+    temp = pokemon.groupby(["pokedex_number", "pokemon"]).agg({"move_id": list}).reset_index()
+    temp = temp.drop(columns=["pokemon"]).rename(columns={"move_id": "move_ids"})
+    pokemon = pd.merge(pokemon, temp, on="pokedex_number").drop(columns=["pokemon_id", "move_id"]).drop_duplicates(subset=["pokedex_number"])
+    print(pokemon)
 
     ######### RENAME ID COLUMNS TO _ID #########
     pokemon = pokemon.rename(columns={"pokedex_number": "_id"})
     types = types.rename(columns={"id": "_id"})
+    moves = moves.rename(columns={"id": "_id"})
 
     ######### CONVERT FROM DATAFRAME TO DICTS #########
     pokemon = pokemon.to_dict("records")
     types = types.to_dict("records")
+    moves = moves.to_dict("records")
 
     ######### SEND TO MONGODB #########
     mongodb_client.drop_database("pokemon")
@@ -32,6 +42,7 @@ def mySQLtoMongoDB():
     
     db.pokemon.insert_many(pokemon)
     db.types.insert_many(types)
+    db.moves.insert_many(moves)
 
     print("sucessfully uploaded data to MongoDB server")
 
