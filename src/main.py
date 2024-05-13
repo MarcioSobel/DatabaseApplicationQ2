6b4c0646459c56072dcd66e13832c6d4f6b2f889
marciosobel <marcinhosobel@hotmail.com>
2024-05-13 03:42:26 -0300

    add MySQL -> MongoDB option

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
 import convertToFile
 import convertToServer
+import mySqlToMongo
 
-answer = input("Do you want to convert to SQL server (recommended)? (Y/n)\n> ")
+print("Choose your option:")
 
-if (answer.lower() == "n"):
-    print("Converting into /convertedData...")
-    convertToFile.csvToSQL()
-else:
-    print("Converting to your local SQL server...")
-    convertToServer.csvToSQL()
-    
+options = ["Convert CSVs to SQL files", "Convert CSVs to SQL Server", "Migrate from MySQL to MongoDB"]
+for i in range(len(options)):
+    print(f"{i+1}. {options[i]}")
+
+answer = ""
+while True:
+    answer = input("\n> ")
+
+    try:
+        answer = int(answer)
+        if answer < 1 or answer > len(options):
+            raise Exception()
+    except:
+        print("Please insert a valid option")
+        continue
+    break
+
+print("Operating...")
+match (answer):
+    case 1:
+        convertToFile.csvToSQL()
+    case 2:
+        convertToServer.csvToSQL()
+    case 3:
+        mySqlToMongo.mySQLtoMongoDB()
